57e172b2535be30c5f3d82c277bbbb5367ac1685
Luis Henrique Siva Ferreira <l.henrick@live.com>
2025-03-19 21:16:53 -0300

    utilizando_'Column'_para_permitir_varias_linhas_e_criando_linhas_de_'icones'

--- layouts/lib/main.dart
+++ layouts/lib/main.dart
@@ -12,27 +12,59 @@ class MeuApp extends StatelessWidget {
           title: Text('Layouts')
         ),
         
-        body: Container(   // widget 'Container e Center' só é possível adiçionar apenas um filho 'child'
-          color: Colors.green,
-          child: Row(   // 'Row' é uma linha horizontal, que permite inserir quantos itens forem necessários
-            children: [
-              Expanded(   // 'Expanded' divide a area pela quantidade de filhos 'child' que 'children' contém
-                child: Container(
-                  child: Text('NOME'),
+        body: Column(
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.min,   // centraliza a linha de icones no centro da tela
+              children: [
+                Icon(Icons.star, color: Colors.green),    // icone 'estrela'
+                Icon(Icons.star, color: Colors.green),
+                Icon(Icons.star, color: Colors.green),
+                Icon(Icons.star, color: Colors.green),
+                Icon(Icons.star, color: Colors.green)
+              ]
+            ),
+
+            Row(   // 'Row' é uma linha horizontal, que permite inserir quantos itens forem necessários
+              children: [
+                Expanded(   // 'Expanded' divide a area pela quantidade de filhos 'child' que 'children' contém
+                  child: Container(
+                    child: Text('NOME'),
+                  ),
                 ),
-              ),
-              Expanded(
-                child: Container(
-                  child: Text('SOBRENOME'),
+                Expanded(
+                  child: Container(
+                    child: Text('SOBRENOME'),
+                  ),
                 ),
-              ),
-              Expanded(
-                child: Container(
-                  child: Text('IDADE'),
+                Expanded(
+                  child: Container(
+                    child: Text('IDADE'),
+                  ),
                 ),
-              ),
-            ],
-          )
+              ],
+            ),
+
+            Row(    // outra linha
+              children: [
+                Expanded(
+                  child: Container(
+                    child: Text('Luis Henrique'),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    child: Text('Silva Ferreira'),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    child: Text('33 anos'),
+                  ),
+                ),
+              ],
+            )
+          ]
         )
       )
     );
