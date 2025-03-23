74b25ed9915467c5f44c1abb8eee5e0d6111b1ac
Luis Henrique Siva Ferreira <l.henrick@live.com>
2025-03-22 21:28:55 -0300

    finalizando_tela_criando_area_de_'descricao'_e_transformado_Expanded_em_componente_para_evitar_a_repeticao_de_codigos

--- criacao_de_tela/lib/main.dart
+++ criacao_de_tela/lib/main.dart
@@ -1,41 +1,51 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MeuApp() );
+void main() => runApp(MeuApp());
 
 class MeuApp extends StatelessWidget {
+
+  callAction() {
+
+  }
+
+  mapAction() {
+
+  }
+
+  shareAction() {
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Criando Tela')
-        ),
-        body:Column(
+        appBar: AppBar(title: Text('Criando Tela')),
+        body: Column(
           children: <Widget>[
-            Image(image: AssetImage('assets/muro.jpg')),     // inserindo imagem 
+            Image(image: AssetImage('assets/muro.jpg')),    // inserindo imagem
             Container(
               padding: EdgeInsets.all(20),
-              child:Row(
-                children:[
+              child: Row(
+                children: [
                   Expanded(
                     child: Column(    // campo de informações
-                      crossAxisAlignment: CrossAxisAlignment.start, // Alinha os textos à esquerda
+                      crossAxisAlignment: CrossAxisAlignment.start,     // alinha os textos à esquerda
                       children: [
                         Text(
                           'Muro das lamentações',
-                        style: TextStyle(fontSize:18, fontWeight:FontWeight.bold, color:Colors.black)
+                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
                         ),
                         Text(
                           'Jerusalém, Israel',
-                        style: TextStyle(fontSize:16, color:Colors.grey)
+                          style: TextStyle(fontSize: 16, color: Colors.grey),
                         ),
                       ],
                     ),
                   ),
                   Row(
                     children: <Widget>[
-                      Icon(Icons.star, color:Colors.blue),
+                      Icon(Icons.star, color: Colors.blue),
                       Text('9.65')
                     ],
                   )
@@ -44,45 +54,61 @@ class MeuApp extends StatelessWidget {
             ),
             Container(
               padding: EdgeInsets.all(20),
-              child:Row(
-                children:[
-                  Expanded(
-                    child: TextButton(
-                      onPressed:(){},
-                      child:Column(
-                        children:[
-                          Icon(Icons.call, color:Colors.blue),    // inserção de icone
-                          Text('Ligar')
-                        ]
-                      )
-                    )
+              child: Row(
+                children: [
+                  Botao(    // chamando componente Botao
+                    icon: Icons.call,    // passando propriedades
+                    text: 'Ligar',
+                    onPressed:callAction,
                   ),
-                  Expanded(
-                    child: TextButton(
-                      onPressed:(){},
-                      child:Column(
-                        children:[
-                          Icon(Icons.location_on, color:Colors.blue),
-                          Text('Mapa')
-                        ]
-                      )
-                    )
+                  Botao(
+                    icon: Icons.location_on,
+                    text: 'Mapa',
+                    onPressed:mapAction,
                   ),
-                  Expanded(
-                    child: TextButton(
-                      onPressed:(){},
-                      child:Column(
-                        children:[
-                          Icon(Icons.share, color:Colors.blue),
-                          Text('Compartilh')
-                        ]
-                      )
-                    )
+                  Botao(
+                    icon: Icons.share,
+                    text: 'Compartilh',
+                    onPressed:shareAction,
                   ),
-                ]
-              )
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                'O Muro das Lamentações, também conhecido como Kotel em hebraico, é um dos locais mais sagrados do judaísmo. '
+                'Ele é uma parede de pedras remanescente do Segundo Templo de Jerusalém, que foi destruído pelos romanos no ano 70 d.C.',
+              ),
             )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class Botao extends StatelessWidget {     // componente Botao
+  Botao({
+    this.onPressed,
+    required this.icon,
+    required this.text,
+  });
+
+  final void Function()? onPressed;
+  final IconData icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TextButton(
+        onPressed: onPressed,     // atribuindo corretamente a função 'onPressed'
+        child: Column(
+          children: [
+            Icon(icon, color: Colors.blue),
+            Text(text)
+          ]
         )
       )
     );
